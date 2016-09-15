f64bbb6720e44be805bbd870b80a974257657fd2
Waterstrong <sqlin@thoughtworks.com>
2016-09-15 23:38:57 +0800

    add publish subscribe demo

---
+++ src/main/java/ws/demo/rabbitmq/message/publishsubscribe/EmitLog.java
@@ -0,0 +1,30 @@
+package ws.demo.rabbitmq.message.publishsubscribe;
+
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLog {
+    private static final String EXCHANGE_NAME = "logs.exchange";
+
+    public static void main(String[] argv)
+            throws java.io.IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.99.100");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+        String message = "Hello Topic!";
+
+        channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
