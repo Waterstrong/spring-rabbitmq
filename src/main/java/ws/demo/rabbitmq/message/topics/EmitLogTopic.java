37eb59a6740ca2fd165728503fc8e11655678d47
Waterstrong <sqlin@thoughtworks.com>
2016-09-16 20:19:22 +0800

    add topic demo

---
+++ src/main/java/ws/demo/rabbitmq/message/topics/EmitLogTopic.java
@@ -0,0 +1,31 @@
+package ws.demo.rabbitmq.message.topics;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLogTopic {
+    private static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] argv)
+            throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.99.100");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+
+//        String routingKey = getRouting(argv);
+//        String message = getMessage(argv);
+
+        String routingKey = "kern.critical";
+        String message = "A critical kernel error";
+
+        channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes());
+        System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+
+        connection.close();
+    }
+}
